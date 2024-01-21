c16492bb8d2532aa704c24234e9d7ac12f86a6bf
cat <contact@catstorm.de>
2024-01-21 16:41:05 +0100

    Added stairs, slab and wall testing block

--- common/src/main/java/notenoughbricks/neb/NotEnoughBricks.java
+++ common/src/main/java/notenoughbricks/neb/NotEnoughBricks.java
@@ -2,8 +2,8 @@ package notenoughbricks.neb;
 
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.block.*;
 import net.minecraft.block.AbstractBlock.Settings;
-import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.RegistryKeys;
@@ -12,19 +12,39 @@ import net.minecraft.registry.RegistryKeys;
 public class NotEnoughBricks {
     public static final String MOD_ID = "neb";
 
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, RegistryKeys.BLOCK);
+    public static final DeferredRegister<Item> BLOCK_ITEMS = DeferredRegister.create(MOD_ID, RegistryKeys.ITEM);
+
     public static void init() {
-        final DeferredRegister<Block> BLOCKS = DeferredRegister.create(MOD_ID, RegistryKeys.BLOCK);
-        final DeferredRegister<Item> BLOCK_ITEMS = DeferredRegister.create(MOD_ID, RegistryKeys.ITEM);
 
         //BLOCKS
         final RegistrySupplier<Block> help = BLOCKS.register("help", () ->
-            new Block(Settings.create().strength(4f)));
+            new Block(Settings.create()));
+
+        final RegistrySupplier<Block> help_stairs = BLOCKS.register("help_stairs", () ->
+            new StairsBlock(help.get().getDefaultState(), Settings.create()));
+
+        final RegistrySupplier<Block> help_slab = BLOCKS.register("help_slab", () ->
+            new SlabBlock(Settings.create()));
+
+        final RegistrySupplier<Block> help_wall = BLOCKS.register("help_wall", () ->
+            new WallBlock(Settings.create()));
 
         BLOCKS.register();
 
         //ITEMS
         final RegistrySupplier<BlockItem> help_item = BLOCK_ITEMS.register("help", () ->
-            new BlockItem(help.get(), new Item.Settings().maxCount(69)));
+            new BlockItem(help.get(), new Item.Settings()));
+
+        final RegistrySupplier<BlockItem> help_stairs_item = BLOCK_ITEMS.register("help_stairs", () ->
+            new BlockItem(help_stairs.get(), new Item.Settings()));
+
+        final RegistrySupplier<BlockItem> help_slab_item = BLOCK_ITEMS.register("help_slab", () ->
+            new BlockItem(help_slab.get(), new Item.Settings()));
+
+        final RegistrySupplier<BlockItem> help_wall_item = BLOCK_ITEMS.register("help_wall", () ->
+            new BlockItem(help_wall.get(), new Item.Settings()));
 
         BLOCK_ITEMS.register();
 
